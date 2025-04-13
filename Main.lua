572d6e424de1b8b35835f16ee968f6bec489410a
h4llstar <jadenblackerson@gmail.com>
2025-04-13 19:18:07 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,138 +1,250 @@
-local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source.lua"))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-	Name = "BGSIHub",
-	LoadingTitle = "Loading...",
-	LoadingSubtitle = "Bubble Gum Simulator INFINITY",
+	Name = "BGSI Hub",
+	Icon = 0,
+	LoadingTitle = "BubbleGum Simulator Infinity Cheat",
+	LoadingSubtitle = "Loading...",
+	Theme = "Default",
+
+	DisableRayfieldPrompts = false,
+	DisableBuildWarnings = false,
+
 	ConfigurationSaving = {
-		Enabled = false
+		Enabled = false,
+		FolderName = nil,
+		FileName = "BGSI Hub"
+	},
+
+	Discord = {
+		Enabled = false,
+		Invite = "noinvitelink",
+		RememberJoins = true
+	},
+
+	KeySystem = false,
+	KeySettings = {
+		Title = "Untitled",
+		Subtitle = "Key System",
+		Note = "No method of obtaining the key is provided",
+		FileName = "Key",
+		SaveKey = true,
+		GrabKeyFromSite = false,
+		Key = {"Hello"}
 	}
 })
 
-local MainTab = Window:CreateTab("Auto Farm")
-local autoHatch = false
-local autoGum = false
-local autoSell = false
-local selectedEgg = nil
-local eggList = {}
-local currentDropdown
-
-local function refreshEggList()
-	local newList = {}
-	local overworld = workspace:FindFirstChild("Worlds"):FindFirstChild("The Overworld")
-	if overworld then
-		local islands = overworld:FindFirstChild("Islands")
-		if islands then
-			for _, island in ipairs(islands:GetChildren()) do
-				local eggsFolder = island:FindFirstChild("Eggs")
-				if eggsFolder then
-					for _, egg in ipairs(eggsFolder:GetChildren()) do
-						table.insert(newList, egg.Name)
-					end
-				end
+local ColFolder = workspace
+for _,folder in pairs(workspace.Rendered:GetChildren()) do
+	if folder.Name == "Chunker" then
+		for _,v in pairs(folder:GetChildren()) do
+			if string.find(v.Name, "-") then
+				ColFolder = folder
 			end
 		end
 	end
-
-	if currentDropdown then
-		currentDropdown:SetOptions(newList)
-	end
-
-	eggList = newList
-	if not selectedEgg and #eggList > 0 then
-		selectedEgg = eggList[1]
-	end
 end
 
-currentDropdown = MainTab:CreateDropdown({
-	Name = "Select Egg",
-	Options = eggList,
-	CurrentOption = selectedEgg or "None",
-	Callback = function(egg)
-		selectedEgg = egg
-	end
+local FarmTab = Window:CreateTab("Farm", "rewind")
+
+local AutoBlowOn = false
+local AutoBlow = FarmTab:CreateToggle({
+	Name = "Auto Blow",
+	Value = false,
+	Flag = "Toggle1",
+	Callback = function(Value)
+		AutoBlowOn = Value
+	end,
 })
 
-refreshEggList()
-
-task.spawn(function()
-	while task.wait(5) do
-		refreshEggList()
-	end
-end)
-
-MainTab:CreateToggle({
-	Name = "Auto Hatch",
-	CurrentValue = false,
-	Callback = function(val)
-		autoHatch = val
-	end
-})
-
-MainTab:CreateToggle({
-	Name = "Auto Bubble",
-	CurrentValue = false,
-	Callback = function(val)
-		autoGum = val
-	end
-})
-
-MainTab:CreateToggle({
+local AutoSellOn = false
+local AutoSell = FarmTab:CreateToggle({
 	Name = "Auto Sell",
-	CurrentValue = false,
-	Callback = function(val)
-		autoSell = val
-	end
+	Value = false,
+	Flag = "Toggle2",
+	Callback = function(Value)
+		AutoSellOn = Value
+	end,
 })
 
-task.spawn(function()
-	while task.wait(1.5) do
-		if autoHatch and selectedEgg then
-			local hatchCount = 1
-			pcall(function()
-				local data = require(game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("GameUI"):WaitForChild("EggUI")).EggData
-				hatchCount = data.MaxMulti or 1
-			end)
+local AutoFarmOn = false
+local AutoFarm = FarmTab:CreateToggle({
+	Name = "Auto Сollect",
+	Value = false,
+	Flag = "Toggle100",
+	Callback = function(Value)
+		AutoFarmOn = Value
+	end,
+})
 
-			game:GetService("ReplicatedStorage")
-				:WaitForChild("Shared")
-				:WaitForChild("Framework")
-				:WaitForChild("Network")
-				:WaitForChild("Remote")
-				:WaitForChild("Event")
-				:FireServer("HatchEgg", selectedEgg, hatchCount)
+local ItemsTab = Window:CreateTab("Items", "rewind")
+
+local AutoDoggyOn = false
+local AutoDoggy = ItemsTab:CreateToggle({
+	Name = "Auto Doggy Game",
+	Value = false,
+	Flag = "Toggle3",
+	Callback = function(Value)
+		AutoDoggyOn = Value
+	end,
+})
+
+local AutoPlaytimeOn = false
+local AutoPlaytime = ItemsTab:CreateToggle({
+	Name = "Auto Collect Playtime",
+	Value = false,
+	Flag = "Toggle5",
+	Callback = function(Value)
+		AutoPlaytimeOn = Value
+	end,
+})
+
+local AutoChestsOn = false
+local AutoChests = ItemsTab:CreateToggle({
+	Name = "Auto Collect Chests",
+	Value = false,
+	Flag = "Toggle69",
+	Callback = function(Value)
+		AutoChestsOn = Value
+	end,
+})
+
+local AutoShopOn = false
+local AutoShop = ItemsTab:CreateToggle({
+	Name = "Auto Alien Shop",
+	Value = false,
+	Flag = "Toggle89",
+	Callback = function(Value)
+		AutoShopOn = Value
+	end,
+})
+
+local AutoTicketOn = false
+local AutoTicket = ItemsTab:CreateToggle({
+	Name = "Auto Claim Wheel Tickets",
+	Value = false,
+	Flag = "Toggle101",
+	Callback = function(Value)
+		AutoTicketOn = Value
+	end,
+})
+
+local AutoWheelOn = false
+local AutoWheel = ItemsTab:CreateToggle({
+	Name = "Auto Spin Wheel",
+	Value = false,
+	Flag = "Toggle102",
+	Callback = function(Value)
+		AutoWheelOn = Value
+	end,
+})
+
+local EggsTab = Window:CreateTab("Eggs", "rewind")
+local EggsText = EggsTab:CreateSection("This is necessary to open the eggs a little faster.")
+
+local SelectedEgg = ""
+local Eggs = {}
+for _,egg in pairs(game.ReplicatedStorage.Assets.Eggs:GetChildren()) do
+	if not string.find(egg.Name, "Golden") and not string.find(egg.Name, "Season") and not string.find(egg.Name, "Shop") and egg.Name ~= "PackageLink" then
+		table.insert(Eggs, egg.Name)
+	end
+end
+local SelectEgg = EggsTab:CreateDropdown({
+	Name = "Select Egg",
+	Options = Eggs, 
+	CurrentOption = {},
+	MultipleOptions = false,
+	Flag = "Dropdown1",
+	Callback = function(CurrentOption)
+		for _,i in pairs(CurrentOption) do 
+			SelectedEgg = i
+		end
+	end,
+})
+
+local CurrentEggsAmount = 1
+local EggsAmount = EggsTab:CreateSlider({
+	Name = "How many eggs to open",
+	Range = {1, 6},
+	Increment = 1,
+	Suffix = "Eggs",
+	CurrentValue = 1,
+	Flag = "Slider1",
+	Callback = function(CurrentValue)
+		CurrentEggsAmount = CurrentValue
+	end,
+})
+
+local AutoEggOn = false
+local AutoEgg = EggsTab:CreateToggle({
+	Name = "Open Eggs",
+	Value = false,
+	Flag = "Toggle4",
+	Callback = function(Value)
+		AutoEggOn = Value
+		if Value then
+			Rayfield:Notify({
+				Title = "Eggs Open",
+				Content = "To open eggs, you need to stand next to them",
+				Duration = 3,
+				Image = "rewind",
+			})
+		end
+	end,
+})
+
+local while1 = coroutine.create(function()
+	while wait(10) do
+		if AutoChestsOn then
+			for _,chest in pairs(workspace.Rendered.Chests:GetChildren()) do
+				if not workspace.Rendered.Generic:FindFirstChild(chest.Name):FindFirstChild("ChestRespawn") then
+					game.ReplicatedStorage.Shared.Framework.Network.Remote.Event:FireServer("ClaimChest", chest.Name, true)
+				end
+			end
+		end
+		if AutoShopOn then
+			for i = 1, 3 do
+				for v = 1, 12 do
+					game.ReplicatedStorage.Shared.Framework.Network.Remote.Event:FireServer("BuyShopItem", "alien-shop", i)
+					wait(0.1)
+				end
+			end
+		end
+		if AutoPlaytimeOn then
+			for i = 1,9 do
+				game.ReplicatedStorage.Shared.Framework.Network.Remote.Function:InvokeServer("ClaimPlaytime", i)
+			end
+		end
+		if AutoDoggyOn then
+			game.ReplicatedStorage.Shared.Framework.Network.Remote.Event:FireServer("DoggyJumpWin", 3)
+		end
+		if AutoTicketOn then
+			game.ReplicatedStorage.Shared.Framework.Network.Remote.Event:FireServer("ClaimFreeWheelSpin")
+		end
+		if AutoWheelOn then
+			game.ReplicatedStorage.Shared.Framework.Network.Remote.Function:InvokeServer("WheelSpin")
 		end
 	end
 end)
+coroutine.resume(while1)
 
-task.spawn(function()
-	while task.wait(0.1) do
-		if autoGum then
-			pcall(function()
-				game:GetService("ReplicatedStorage")
-					:WaitForChild("Shared")
-					:WaitForChild("Framework")
-					:WaitForChild("Network")
-					:WaitForChild("Remote")
-					:WaitForChild("Event")
-					:FireServer("BlowBubble")
-			end)
+local while2 = coroutine.create(function()
+	while wait(0.1) do
+		if AutoFarmOn then
+			for _,v in pairs(ColFolder:GetChildren()) do
+				game.ReplicatedStorage.Remotes.Pickups.CollectPickup:FireServer(v.Name)
+				v:Destroy()
+			end
+		end
+		if AutoBlowOn then
+			game.ReplicatedStorage.Shared.Framework.Network.Remote.Event:FireServer("BlowBubble")
+		end
+		if AutoSellOn then
+			game.ReplicatedStorage.Shared.Framework.Network.Remote.Event:FireServer("SellBubble")
+		end
+		if AutoEggOn then
+			game.ReplicatedStorage.Shared.Framework.Network.Remote.Event:FireServer("HatchEgg", SelectedEgg, CurrentEggsAmount)
 		end
 	end
 end)
-
-task.spawn(function()
-	while task.wait(0.1) do
-		if autoSell then
-			pcall(function()
-				game:GetService("ReplicatedStorage")
-					:WaitForChild("Shared")
-					:WaitForChild("Framework")
-					:WaitForChild("Network")
-					:WaitForChild("Remote")
-					:WaitForChild("Event")
-					:FireServer("SellBubble")
-			end)
-		end
-	end
-end)
+coroutine.resume(while2)
