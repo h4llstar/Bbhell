282f9ef21d5505c96c84a553519cf683679d5c0d
h4llstar <jadenblackerson@gmail.com>
2025-05-17 17:48:36 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -162,7 +162,7 @@ EggsTab:CreateDropdown({
 local CurrentEggsAmount = 1
 EggsTab:CreateSlider({
     Name = "How many eggs to open",
-    Range = {1, 6},
+    Range = {1, 17},
     Increment = 1,
     Suffix = "Eggs",
     CurrentValue = 1,
